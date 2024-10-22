c9599d4dec4520922bc9f87b52e172db6a850a2b
divrawat <divrawat2001@gmail>
2024-10-22 19:15:08 +0530

    sef0seg5

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,10 @@ import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(() => {
-    const disableRightClick = (event) => { event.preventDefault(); };
-    document.addEventListener('contextmenu', disableRightClick);
-  }, []);
+  // useEffect(() => {
+  //   const disableRightClick = (event) => { event.preventDefault(); };
+  //   document.addEventListener('contextmenu', disableRightClick);
+  // }, []);
 
 
   return (
